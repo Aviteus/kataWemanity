8d6ce2fce9828504bda1b2196cf88d7c9120b899
Kevin <kevin.legay@socgen.com>
2017-11-21 15:51:56 +0100

    add test for Model

--- src/test/java/model/AccountTest.java
+++ src/test/java/model/AccountTest.java
@@ -2,14 +2,41 @@ package model;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+
 import org.junit.Test;
 
 public class AccountTest {
-	
-    private Account sut;
+
+	private Account sut;
+
+	private int scale = 2;
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void shouldBeZeroAtTheBeginning() {
+		// Input
+		BigDecimal expected = BigDecimal.ZERO;
+		expected = expected.setScale(scale);
+		// Call
+		BigDecimal actual = sut.getAmount();
+		// Assert
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void shouldGettheSettedValue() {
+		// Input
+		BigDecimal expected = new BigDecimal(1);
+		expected.setScale(scale);
+		BigDecimal input = new BigDecimal(1);
+		input = input.setScale(scale);
+		// Call
+		sut.setAmount(input);
+		BigDecimal actual = sut.getAmount();
+		// Assert
+		assertEquals(expected, actual);
 	}
 
 }
